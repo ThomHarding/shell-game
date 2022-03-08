c169ecf0b54b1bc617ffe0d8cc36094336dd7e5d
THOMAS HARDING <hardingt604@gmail.com>
2022-03-08 11:56:55 -0700

    added first draft of resetStyles, getRandomItem, and handleGuess. still no event listeners, though

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-// import functions and grab DOM elements
 let buttonOne = document.getElementById('cup-one-button');
 let buttonTwo = document.getElementById('cup-two-button');
 let buttonThree = document.getElementById('cup-three-button');
@@ -6,25 +5,40 @@ let buttonThree = document.getElementById('cup-three-button');
 let imageOne = document.getElementById('cup-one-img');
 let imageTwo = document.getElementById('cup-two-img');
 let imageThree = document.getElementById('cup-three-img');
+let imageArray = document.querySelectorAll('.cup-image');
 
 let totalDisplay = document.getElementById('total-guesses');
 let correctDisplay = document.getElementById('correct-guesses');
 let incorrectDisplay = document.getElementById('incorrect-guesses');
 
-// let state
-function resetStyles() {
+var totalGuesses = 0;
+var correctGuesses = 0;
 
+function resetStyles() {
+    imageOne.src = './assets/base-cup.png';
+    imageTwo.src = './assets/base-cup.png';
+    imageThree.src = './assets/base-cup.png';
 }
 
 function getRandomItem(arr) {
-
+    const index = Math.floor(Math.random() * arr.length);
+    return arr[index];
 }
 
 function handleGuess(userGuess, correctSpot) {
-
+    resetStyles();
+    totalGuesses++;
+    if (userGuess === correctSpot) {
+        correctGuesses++;
+    }
+    for (let cup of imageArray) {
+        if (cup !== correctSpot) {
+            cup.src = './assets/incorrect-cup.png';
+        } else {
+            cup.src = './assets/correct-cup.png';
+        }
+    }
+    totalDisplay.textContent = totalGuesses;
+    correctDisplay.textContent = correctGuesses;
+    incorrectDisplay.textContent = totalGuesses - correctGuesses;
 }
-
-// set event listeners 
-  // get user input
-  // use user input to update state 
-  // update DOM to reflect the new state
