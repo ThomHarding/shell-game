2e896b234169e9be031bd8e711130c97ec01e34b
THOMAS HARDING <hardingt604@gmail.com>
2022-03-08 11:43:27 -0700

    added javascript outline with basic required elements

--- app.js
+++ app.js
@@ -1,6 +1,28 @@
 // import functions and grab DOM elements
+let buttonOne = document.getElementById('cup-one-button');
+let buttonTwo = document.getElementById('cup-two-button');
+let buttonThree = document.getElementById('cup-three-button');
+
+let imageOne = document.getElementById('cup-one-img');
+let imageTwo = document.getElementById('cup-two-img');
+let imageThree = document.getElementById('cup-three-img');
+
+let totalDisplay = document.getElementById('total-guesses');
+let correctDisplay = document.getElementById('correct-guesses');
+let incorrectDisplay = document.getElementById('incorrect-guesses');
 
 // let state
+function resetStyles() {
+
+}
+
+function getRandomItem(arr) {
+
+}
+
+function handleGuess(userGuess, correctSpot) {
+
+}
 
 // set event listeners 
   // get user input
